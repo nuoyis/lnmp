2cf5ec365fca8a4a74679057a3debdd51dd2ac8b
Xu Weikang <78991709+nuoyis@users.noreply.github.com>
2025-08-29 02:06:15 +0800

    修复问题

--- start-dev/build.sh
+++ start-dev/build.sh
@@ -3,4 +3,4 @@
 # 编写日期: 2025-03-31
 # dockerfile构建测试专用
 
-docker build -f ../dockerfile -t nuoyis1024/nuoyis-lnmp:dev --no-cache -f dockerfile --load ../
+docker build -f ../dockerfile -t nuoyis1024/lnmp:dev --no-cache -f dockerfile --load ../
